2788a311658cb6dc92f456e4e54ba0cac06eb2fd
AJAY12398 <ajay.mandaviya03@gmail.com>
2021-09-28 00:27:25 +0530

    add routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+import Comment from "./component/Comment";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import PostProvider from "./context/postContext";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <PostProvider>
+        <BrowserRouter>
+        <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path={"/post/:id"} component={Comment} />
+          </Switch>
+        </BrowserRouter>
+      </PostProvider>
     </div>
   );
 }
