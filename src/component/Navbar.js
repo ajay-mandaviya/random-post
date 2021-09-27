9124016b4442fe1079caf49d1d4958fb8dac05d9
AJAY12398 <ajay.mandaviya03@gmail.com>
2021-09-28 00:29:13 +0530

    add navbar

---
+++ src/component/Navbar.js
@@ -0,0 +1,24 @@
+import React  from 'react';
+const Navbar = () => {
+    return (
+        <div className = "navbar">
+            <div>
+            <h4>
+                JSON
+            </h4>
+            </div>
+            <div>            
+            <ul>
+              
+                <li><h5>Home</h5></li>
+                <li><h5>Menu</h5></li>
+                <li><h5>More</h5></li>
+                <li><h5>About us</h5></li>
+            </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
+
